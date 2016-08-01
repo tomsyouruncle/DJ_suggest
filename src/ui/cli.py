8592323e7d98b21c386c6b5db4dbddc880d9ffa9
tomsyouruncle <thomas.e.turner@gmail.com>
2016-08-01 12:13:34 +0100

    Refactor CLI

--- src/ui/cli.py
+++ src/ui/cli.py
@@ -46,16 +46,17 @@ init_seed = define_seed()
 print("Generating recommendations...")
 training_set = define_training_set(init_seed)
 suggest_set = band_BPMs(get_new_recs_and_feats(init_seed,30),80,170)
-gui_cols = ['artist_name','track_name','tempo','uri','status','P_accept']
+gui_cols = ['artist_name','track_name','tempo','uri','status']
 acc_rej = ''
 while acc_rej != 'q':
+  print(' ')
   print('TRAINING SET')
   print('------------')
   print(training_set[gui_cols])
   print(' ')
   print('RECOMMENDATIONS')
   print('---------------')
-  print(suggest_set[gui_cols])
+  print(suggest_set[gui_cols + ['P_accept']])
   print(' ')
   print('Type a# or r# to accept or reject a track.')
   print('(e.g. To accept track 5, type: a5)')
@@ -73,10 +74,11 @@ while acc_rej != 'q':
     suggest_set.to_pickle(os.path.join(PROJ_ROOT,'data','interim','track_set.pkl'))
   elif acc_rej == 'g':
     init_seed = define_seed()
-    suggest_set = generate_recs(init_seed)
+    suggest_set = band_BPMs(get_new_recs_and_feats(init_seed,30),80,170)
   else:
     print('Error, invalid input.')
 
+  # only train the model when it has positive and negative examples
   if training_set.status.min() < 0:
     suggest_set = train_NB_model(suggest_set, training_set)
 
